d4cb8ecf0fdb5a7a98d66eb7141b03b4afe426bc
Minkyeongweb <jangminkyeong99@gmail.com>
2024-10-10 17:20:20 +0900

    2-7 COMMIT

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-//N+1 문제 Fetch Join
+//N+1 문제 Fetch Join 방식 사용
     @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
